4648b0275c84d535809c14457bcb52b9859f1bd2
Taimour17 <140527563+Taimour17@users.noreply.github.com>
2024-08-07 20:25:25 +0500

    Add files via upload

---
+++ Assignment/Calculator.py
@@ -0,0 +1,86 @@
+# Calculator Simple
+
+# # Sum Function
+
+
+print("\nWe are going to do some sumfication\n")
+first_number = int (input("Provide First number for Sum ")) 
+second_number = int (input("Provide second Number for Sum "))
+
+sum = first_number + second_number
+
+print (("\nanswer is "), sum)
+
+## End Sum
+
+# Sub Function
+
+
+print("\nWe are going to do some subtraction Now\n")
+
+
+first_number = int (input("Provide First number for Subtraction "))
+second_number = int (input("Provide second Number for Subtraction "))
+
+sub = first_number - second_number
+
+print (("\nanswer is "), sub)
+
+## End Sub
+
+# Multiply Function
+
+
+print("\nGood, Now lets Try some Multiplication\n")
+
+
+first_number = int (input("Provide First number for Multiplication "))
+second_number = int (input("Provide second Number for Multiplication "))
+
+mul = first_number * second_number
+
+print (("\nanswer is "), mul)
+
+## End Multiply
+
+# Divide Function
+
+print("\nVery Good, Now lets Try some Division\n")
+
+
+first_number = int (input("Provide First number for Divide "))
+second_number = int (input("Provide second Number for Divide "))
+
+div = first_number / second_number
+
+print (("\nanswer is "), div)
+
+## End Divide Function
+
+# Power of Function
+
+print("\nNice, Now lets Try Giving SOme Powers to our Integers\n")
+
+
+first_number = int (input("Provide number Which you want to Give power to "))
+second_number = int (input("Provide Power number "))
+
+Pow = first_number ** second_number
+
+print (("\nanswer is "), Pow)
+
+## End Power of Function
+
+# Floor Division of Function
+
+print("\nNice, Now lets Try Floor Division\n")
+
+
+first_number = int (input("Provide number Which you want to Give power to "))
+second_number = int (input("Provide Power number "))
+
+floor = first_number // second_number
+
+print (("\nanswer is "), floor)
+
+## End floor Function
---
+++ Assignment/Choice.py
@@ -0,0 +1,45 @@
+## interactive Calculator
+
+### Give Choice to User
+
+print ("\n\t\tWhat Function You want To Use")
+
+Choice = int (input("\n\t 1. Summation \t 2. Difference \t 3. Multiplication \t 4. Division \n\n\t 5. Power of \t 6. Remainder \t 7. Floor Division \t 8. Exit\n\n\t"))
+
+if Choice == 8:
+
+    print =("\n\t\tGood Bye, Have a Great Class")
+
+    if Choice == 1:
+
+        ### Getting the Input
+
+        first_number = int (input("\n\tProvide First Number\n"))
+        second_number = int (input("\n\tProvide Second Number\n"))
+
+        ### Performing the Funtion 
+
+        print = ("\n\t\t The Sum Of Two Number Is: \n\t\t", first_number + second_number)
+
+    elif Choice == 2:
+
+        ### Getting the Input
+
+        first_number = int (input("\n\tProvide First Number\n"))
+        second_number = int (input("\n\tProvide Second Number\n"))
+
+        ### Performing the Funtion 
+
+        print = ("\n\t\t The Sum Of Two Number Is: \n\t\t", first_number - second_number)
+
+    elif Choice == 3:
+
+        ### Getting the Input
+
+        first_number = int (input("\n\tProvide First Number\n"))
+        second_number = int (input("\n\tProvide Second Number\n"))
+
+        ### Performing the Funtion 
+
+        print = ("\n\t\t The Sum Of Two Number Is: \n\t\t", first_number * second_number)
+
